f5c48767fc27ff661b87db601aa5d0d55e413cbd
kairox <git@newgenerations.im>
2018-12-15 20:57:41 +0900

    コンテナ変更

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 docker run --rm -v $(pwd):/src -w /src \
-           tjholowaychuk/up-crystal crystal build \
+           jhass/crystal-build-x86_64 crystal build \
            --link-flags -static -o bootstrap src/main.cr && \
 chmod 755 bootstrap && \
 zip lambda-crystal.zip bootstrap
